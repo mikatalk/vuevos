036e30b50c31b18650d105d45f49f70d1c1b1e61
Mika I <mikatalk@users.noreply.github.com>
2021-04-13 20:22:06 -0700

    Fix deployment

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,12 @@
 
 cd ./dist
 mkdir tmp
-mkdir tmp/.git
 cp -r ../.git tmp/.git
 cd tmp
-git checkout gh-pages
+git checkout -b gh-pages
 cd ..
 cp -r tmp/.git .git
 rm -rf tmp
-rm app.html
 echo "vuevos.com" > CNAME
 git add .
 git commit -m "Deployment"
